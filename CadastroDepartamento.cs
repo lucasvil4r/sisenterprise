d54208ffb1214d795e50057b14262a963b6e283f
Gustavo Gatto <guzin202@gmail.com>
2023-09-30 22:32:44 -0300

    Feature: Inclui o cadastro dos departamentos

--- CadastroDepartamento.cs
+++ CadastroDepartamento.cs
@@ -21,5 +21,42 @@ namespace SisEnterprise
 		{
 
 		}
-	}
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CadastroDepartamento_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'sisenterpriseDataSet1.Cadastro_Departamento'. Você pode movê-la ou removê-la conforme necessário.
+            this.cadastro_DepartamentoTableAdapter.Fill(this.sisenterpriseDataSet1.Cadastro_Departamento);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
