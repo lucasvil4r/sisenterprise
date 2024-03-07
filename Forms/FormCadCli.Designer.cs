8852ac2514720421f37a1dc4c210214981c83bd9
lucasvil4r <lucasvilar-celestino@hotmail.com>
2024-03-07 17:56:54 -0300

    feature: começando criar tela de cadastro de cliente

--- Forms/FormCadCli.Designer.cs
+++ Forms/FormCadCli.Designer.cs
@@ -28,36 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBoxRazao = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonDeletar = new System.Windows.Forms.Button();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonAdicionar = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBoxRazao = new System.Windows.Forms.MaskedTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBoxCodigo = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.maskedTextBoxCodigo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.maskedTextBoxRazao);
+            this.groupBox1.Location = new System.Drawing.Point(35, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(861, 468);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro de Cliente";
+            // 
+            // buttonDeletar
+            // 
+            this.buttonDeletar.Location = new System.Drawing.Point(419, 511);
+            this.buttonDeletar.Name = "buttonDeletar";
+            this.buttonDeletar.Size = new System.Drawing.Size(105, 31);
+            this.buttonDeletar.TabIndex = 5;
+            this.buttonDeletar.Text = "Excluir";
+            this.buttonDeletar.UseVisualStyleBackColor = true;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(293, 511);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(105, 31);
+            this.buttonSalvar.TabIndex = 4;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdicionar
+            // 
+            this.buttonAdicionar.Location = new System.Drawing.Point(545, 511);
+            this.buttonAdicionar.Name = "buttonAdicionar";
+            this.buttonAdicionar.Size = new System.Drawing.Size(105, 31);
+            this.buttonAdicionar.TabIndex = 6;
+            this.buttonAdicionar.Text = "Adicionar";
+            this.buttonAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Location = new System.Drawing.Point(671, 511);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(105, 31);
+            this.buttonLimpar.TabIndex = 7;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Criar proposta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBoxRazao
+            // 
+            this.maskedTextBoxRazao.AccessibleDescription = "";
+            this.maskedTextBoxRazao.AccessibleName = "";
+            this.maskedTextBoxRazao.Location = new System.Drawing.Point(117, 82);
+            this.maskedTextBoxRazao.Name = "maskedTextBoxRazao";
+            this.maskedTextBoxRazao.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBoxRazao.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(85, 369);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(690, 78);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(198, 233);
+            this.label1.Location = new System.Drawing.Point(41, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Razão Social:";
             // 
-            // textBoxRazao
+            // label2
             // 
-            this.textBoxRazao.Location = new System.Drawing.Point(239, 233);
-            this.textBoxRazao.Name = "textBoxRazao";
-            this.textBoxRazao.Size = new System.Drawing.Size(100, 20);
-            this.textBoxRazao.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Codigo:";
             // 
-            // button1
+            // maskedTextBoxCodigo
             // 
-            this.button1.Location = new System.Drawing.Point(278, 297);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.maskedTextBoxCodigo.AccessibleDescription = "";
+            this.maskedTextBoxCodigo.AccessibleName = "";
+            this.maskedTextBoxCodigo.Location = new System.Drawing.Point(120, 47);
+            this.maskedTextBoxCodigo.Name = "maskedTextBoxCodigo";
+            this.maskedTextBoxCodigo.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBoxCodigo.TabIndex = 4;
             // 
             // FormCadCli
             // 
@@ -65,20 +151,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(939, 560);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBoxRazao);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.buttonAdicionar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.buttonDeletar);
             this.Name = "FormCadCli";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Cliente";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBoxRazao;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonDeletar;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonAdicionar;
+        private System.Windows.Forms.Button buttonLimpar;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxRazao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
